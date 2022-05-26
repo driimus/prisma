9a8023fa5495ef053d97faa9c8ef69353ca24f06
Cristi Petre <driimus@vivaldi.net>
2022-05-26 23:58:39 +0100

    fix(client-types): consistent falsy select for relations

--- packages/client/src/generation/TSClient/Payload.ts
+++ packages/client/src/generation/TSClient/Payload.ts
@@ -27,7 +27,7 @@ export type ${payloadName}<
   > = S extends true
   ? ${name}
   : S extends undefined
-  ? never
+  ? IsRelationalPayload extends true ? undefined : never
   : S extends ${argsName}${findManyArg}
   ? S extends { include: infer ProjectionFilter }
   ? ${name} ${include.length > 0 ? ` & ${include}` : ''}
@@ -62,7 +62,7 @@ ${indent(
       (f) =>
         `P extends '${f.name}' ? ${this.wrapType(
           f,
-          `${getPayloadName((f.outputType.type as DMMF.OutputType).name)}<ProjectionFilter[P]>`,
+          `${getPayloadName((f.outputType.type as DMMF.OutputType).name)}<ProjectionFilter[P], true>`,
         )} :`,
     )
     .join('\n'),
